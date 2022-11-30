3a455a207d5ac77c39847413215662be91ae44d3
Aashrey Sharma <aashrey9sharma@gmail.com>
2022-11-30 23:12:56 +0530

    Updated UI message

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const EVENT_URL_UPDATED = 'URL_UPDATED'
 
 export const MSG_SET_URL = 'Set URL ⚙️'
 export const MSG_GO_TO_LAYER = 'Go to Layer ->'
-export const MSG_OPEN_LINK = 'Open Link ->'
+export const MSG_OPEN_LINK = 'Open Link 🔗'
 
 export const MSG_LAYER_DELETED = 'Linked layer may have been deleted. Please update URL.'
 export const MSG_NAV_TO_LAYER = 'Button will navigate to selected layer.'
