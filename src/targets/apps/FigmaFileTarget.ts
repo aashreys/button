6dfbed4e5cec90c9e8de6331b94b5afe64fdba7c
Aashrey Sharma <aashrey9sharma@gmail.com>
2024-08-19 16:57:50 -0700

    Added Figma Prototype icon and updated existing Figma icons

--- src/targets/apps/FigmaFileTarget.ts
+++ src/targets/apps/FigmaFileTarget.ts
@@ -1,11 +1,11 @@
-import { FIGMA_ICON } from "../../app_icons/figma";
+import { FIGMA_FILE_ICON } from "../../app_icons/figma_file";
 import { Themes } from "../../themes";
 import { Target, TargetType } from "../target";
 
 export class FigmaFileTarget implements Target {
 
   readonly type = TargetType.WEB
-  readonly icon = FIGMA_ICON
+  readonly icon = FIGMA_FILE_ICON
   readonly theme = Themes.FIGMA_BLUE
   readonly message: string = 'Click to open link'
   readonly label: string = 'Open Figma File'
